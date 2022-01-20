c798ac780afe98f537ec3fc526a2106b5cd30058
KanDai <KanDai@users.noreply.github.com>
2022-01-20 11:46:13 +0900

    update: homebrew setup script

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,21 +1,72 @@
 #!/bin/sh
 
 # ========================================
-# Install Cli Command
+# Install Formulae
 # ========================================
 
-brew install curl
+# brew install curl # デフォルトでインストールされている
 brew install wget
 brew install tree
+
+# Heroku Cli
+brew tap heroku/brew && brew install heroku
+
+# Node.js version manager
+brew install fnm
+
+# JavaScript Package Manager
 brew install yarn
-brew install heroku
+
+# runtime for JavaScript and TypeScript
 brew install deno
 
+# php
+
+# rust
+# brew install rustup rust-analyzer
+# rustup-init
+# rustup update
+
 
 # ========================================
-# Install Apllications
+# Install Casks
 # ========================================
 
+# Browser
 brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask microsoft-edge
+brew install --cask biscuit
+
+# Editor
 brew install --cask visual-studio-code
+brew install --cask sublime-text
+brew install --cask typora
+
+# IME
+brew install --cask google-japanese-ime
+
+# Communication
+brew install --cask slack
+brew install --cask discord
+brew install --cask zoom
+
+# Develop
+brew install --cask docker
+brew install --cask local
+brew install --cask sequel-pro
+brew install --cask stoplight-studio
+brew install --cask imageoptim
+
+# Other
+brew install --cask path-finder
+brew install --cask gyazo
+brew install --cask kap
+brew install --cask pixelsnap
 brew install --cask 1password
+brew install --cask alfred
+brew install --cask mapture
+brew install --cask notion
+# brew install --cask dynalist
+# brew install --cask evernote
+# brew install --cask dropbox
